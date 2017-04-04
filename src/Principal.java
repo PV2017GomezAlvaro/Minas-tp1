02bd0c8d9fb789ddc35d47ba95c28e3caa79b5ac
Alumno <Alumno@2CI809B2001F561>
2017-04-04 20:35:47 -0300

    Incorporación del Proyecto al Respositorio

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class Principal {
+    public static void main (String[] alvarito) {
+        System.out.println("Hola mundo");
+    }
+}
